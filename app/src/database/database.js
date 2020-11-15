d5ba62b932978cec962db4cf230139d57f7b1bdb
Merdy <sampekmoy@gmail.com>
2020-11-16 01:04:36 +0300

    add sqlite

--- app/src/database/database.js
+++ app/src/database/database.js
@@ -1,17 +1,33 @@
 const Sequelize = require("sequelize");
 require('dotenv').config()
+const path = require('path')
 
 
-const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: process.env.DB_DIALECT,
-        host: process.env.DB_HOST,
-        logging: false
-    }
-)
+let sequelize
+
+if(process.env.DB_DIALECT === 'mysql') {
+     sequelize = new Sequelize(
+        process.env.DB_DATABASE,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            dialect: process.env.DB_DIALECT,
+            host: process.env.DB_HOST,
+            logging: false
+        }
+    )
+} else if (process.env.DB_DIALECT === 'sqlite') {
+    sequelize = new Sequelize(
+        'localhost',
+        'root',
+        'root',
+        {
+            dialect: 'sqlite',
+            storage: path.join(__dirname, '../..', `${process.env.DB_DATABASE}.sqlite`),
+            logging: false
+        }
+    )
+}
 
 module.exports = {
     sequelize
